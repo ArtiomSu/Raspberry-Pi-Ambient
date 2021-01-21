a62ece6cd56441f2ea89e3258fc499535e2acf0d
ArtiomSu <artiom.sumigora@mycit.ie>
2021-01-21 19:01:11 +0000

    fix firefox not running as root

--- run_firefox.sh
+++ run_firefox.sh
@@ -4,16 +4,16 @@ export DISPLAY=:0.0
 killall firefox-esr
 sleep 2
 echo launching firefox
-firefox-esr $1 &
+sudo -u pi firefox-esr $1 &
 echo firefox launched
 
 echo sleeping for 30 seconds
 sleep 30
 
 echo going full screen
-xdotool key F11
+sudo -u pi xdotool key F11
 
 echo moving mouse
-xdotool mousemove 1080 1920
+sudo -u pi xdotool mousemove 1080 1920
 echo done
 
